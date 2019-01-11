702b9b46547ed29155c04586770e1642c66ce4d8
Matthew Morgan <mmorgan55@cnm.edu>
2019-01-10 23:47:02 -0700

    Added logic for playing pieces and printing to console

--- connect-four/connect4.py
+++ connect-four/connect4.py
@@ -1,23 +1,47 @@
 import numpy as np
 
+COLUMN_COUNT = 6
+ROW_COUNT = 5
+
 
 def create_board():
     board = np.zeros((6, 7))
     return board
 
 
-board = create_board()
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+
+def is_valid_move(board, col):
+    return board[ROW_COUNT][col] == 0
+
+
+def get_next_open_row(board, col):
+    for row in range(ROW_COUNT):
+        if board[row][col] == 0:
+            return row
+
+
+game_board = create_board()
+print(game_board)
 game_over = False
 turn = 0
 
 while not game_over:
     if turn == 0:
-        selection = int(input("Player 1 Make your Selection (0-6):"))
-        print(selection)
-        print(type(selection))
+        column = int(input("Player 1 Make your Selection (0-6):"))
+        if is_valid_move(game_board, column):
+            row = get_next_open_row(game_board, column)
+            drop_piece(game_board, row, column, 1)
+
     else:
-        selection = int(input("Player 2 Make your Selection (0-6):"))
+        column = int(input("Player 2 Make your Selection (0-6):"))
+        if is_valid_move(game_board, column):
+            row = get_next_open_row(game_board, column)
+            drop_piece(game_board, row, column, 2)
+
+    print(np.flip(game_board, 0))
 
     turn += 1
     turn = turn % 2
-    
