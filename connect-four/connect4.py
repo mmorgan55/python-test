49664bb866405e1d27a44bbb6bf5b7b6d54fcf50
Matthew Morgan <mmorgan55@cnm.edu>
2019-01-13 17:21:26 -0700

    Implemented pygame to display a connect four game board

--- connect-four/connect4.py
+++ connect-four/connect4.py
@@ -1,7 +1,11 @@
 import numpy as np
+import pygame
+import sys
 
 COLUMN_COUNT = 7
 ROW_COUNT = 6
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
 
 
 def create_board():
@@ -47,33 +51,64 @@ def find_win(board, piece):
                 return True
 
 
+def draw_board(board):
+    for col in range(COLUMN_COUNT):
+        for row in range(ROW_COUNT):
+            pygame.draw.rect(screen, BLUE,
+                             (col * SQUARE_SIZE, row * SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            pygame.draw.circle(screen, BLACK,
+                               (col * SQUARE_SIZE + SQUARE_SIZE // 2,
+                                row * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE // 2), RADIUS)
+
+
 game_board = create_board()
 print(game_board)
 game_over = False
 turn = 0
 
+pygame.init()
+
+SQUARE_SIZE = 100
+width = COLUMN_COUNT * SQUARE_SIZE
+height = (ROW_COUNT + 1) * SQUARE_SIZE
+RADIUS = SQUARE_SIZE // 2 - 5
+
+size = (width, height)
+
+screen = pygame.display.set_mode(size)
+draw_board(game_board)
+pygame.display.update()
+
 while not game_over:
-    if turn == 0:
-        column = int(input("Player 1 Make your Selection (0-6):"))
-        if is_valid_move(game_board, column):
-            row = get_next_open_row(game_board, column)
-            drop_piece(game_board, row, column, 1)
 
-            if find_win(game_board, 1):
-                print("Worked")
-                game_over = True
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-    else:
-        column = int(input("Player 2 Make your Selection (0-6):"))
-        if is_valid_move(game_board, column):
-            row = get_next_open_row(game_board, column)
-            drop_piece(game_board, row, column, 2)
-            
-            if find_win(game_board, 1):
-                print("Worked")
-                game_over = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print()
 
-    print(np.flip(game_board, 0))
-
-    turn += 1
-    turn = turn % 2
+    # if turn == 0:
+    #     column = int(input("Player 1 Make your Selection (0-6):"))
+    #     if is_valid_move(game_board, column):
+    #         row = get_next_open_row(game_board, column)
+    #         drop_piece(game_board, row, column, 1)
+    #
+    #         if find_win(game_board, 1):
+    #             print("Worked")
+    #             game_over = True
+    #
+    # else:
+    #     column = int(input("Player 2 Make your Selection (0-6):"))
+    #     if is_valid_move(game_board, column):
+    #         row = get_next_open_row(game_board, column)
+    #         drop_piece(game_board, row, column, 2)
+    #
+    #         if find_win(game_board, 1):
+    #             print("Worked")
+    #             game_over = True
+    #
+    # print(np.flip(game_board, 0))
+    #
+    # turn += 1
+    # turn = turn % 2
