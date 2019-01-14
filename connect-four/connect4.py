9d2a099a7c49617737f7aa98aac7b207ce0615fa
Matthew Morgan <mmorgan55@cnm.edu>
2019-01-13 19:43:19 -0700

    Added way to display a piece above board to show where it will be put.

--- connect-four/connect4.py
+++ connect-four/connect4.py
@@ -102,6 +102,15 @@ while not game_over:
         if event.type == pygame.QUIT:
             sys.exit()
 
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, BLACK,(0, 0, width, SQUARE_SIZE))
+            posx = event.pos[0]
+            if turn == 0:
+                pygame.draw.circle(screen, RED, (posx, SQUARE_SIZE // 2), RADIUS)
+            else:
+                pygame.draw.circle(screen, YELLOW, (posx, SQUARE_SIZE // 2), RADIUS)
+        pygame.display.update()
+
         if event.type == pygame.MOUSEBUTTONDOWN:
 
             if turn == 0:
